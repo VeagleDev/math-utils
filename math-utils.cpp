e95932da8c6daae89353b59e553c03129246c76b
LC_Nomade <leopold.courio@outlook.fr>
2022-08-22 17:07:10 +0200

    main.cpp
    premiers cout + premières variables

--- math-utils.cpp
+++ math-utils.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main()
 {
+    cout << "\nBienvenue sur le programme Math-Utils | par LC_Nomade de l'equipe MysteriousDev\n\n";
+    
+    cout << "Quel type de calcul voulez-vous effectuer ?\noperation(op) | arithetique(ar)\ntheoreme(th)  | fonction(f)\nformule(fo)";
+
+    int rep,op,ar,th,f,fo;
+
+    
     
 }
