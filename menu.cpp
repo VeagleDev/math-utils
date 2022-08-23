55e7468040b90f6924cc479b8b58ffd67db91de0
pierrbt <pierre08bt@gmx.co.uk>
2022-08-23 12:24:50 +0200

    correct error

--- menu.cpp
+++ menu.cpp
@@ -19,10 +19,10 @@ int main(int argc, char **argv)
     if(argc == 2)
     {
         string arg = argv[1];
-        if(arg.size() == 2 && isdigit(arg))
+        if(arg.size() == 2 && isdigit(arg[0]) && isdigit(arg[1]))
         {
-            action = stoi(arg); 
-        }          
+            action = stoi(arg);
+        }
     }
 
 
